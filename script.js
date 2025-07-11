f3fafb71fad3da6ef45d50b29e47a68c2f1cf681
Aryan Gupta <124511705+AryanGupta67@users.noreply.github.com>
2025-07-11 21:51:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-// Base Animal class
 class Animal {
   constructor(species) {
     this._species = species;
   }
 
-  // Getter for species
   get species() {
     return this._species;
   }
 
-  // Method to log the sound
   makeSound() {
     console.log(`The ${this.species} makes a sound`);
   }
 }
 
-// Cat class inherits from Animal
 class Cat extends Animal {
   purr() {
     console.log("purr");
   }
 }
 
-// Dog class inherits from Animal
 class Dog extends Animal {
   bark() {
     console.log("woof");
   }
 }
+
+// ⬇️ Attach classes to the window object so Cypress can access them
+window.Animal = Animal;
+window.Cat = Cat;
+window.Dog = Dog;
+
 
