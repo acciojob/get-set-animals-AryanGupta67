c89a7bc95e159ada113aa86535d72f00d51861d5
Aryan Gupta <124511705+AryanGupta67@users.noreply.github.com>
2025-07-11 21:34:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-//complete this code
-class Animal {}
+// Base Animal class
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to log the sound
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
+}
 
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+// Cat class inherits from Animal
+class Cat extends Animal {
+  purr() {
+    console.log("purr");
+  }
+}
+
+// Dog class inherits from Animal
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
+
